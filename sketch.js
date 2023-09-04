1b811d7fe52b4940b199f85f733a71ff228f85d8
loremipsum6321 <loremipsum6321@proton.me>
2023-09-04 14:18:57 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,66 @@
+let width = 896;
+let height = 400;
+let bars = [];
+let barWidth = 5;
+
+function fillArray() {
+  bars = []; // Clear the previous bars
+  for (let i = 0; i < width / barWidth; i++) {
+    bars.push(new Bar(i * barWidth, height, barWidth, -Math.floor(random(0, height))));
+  }
+}
+
+function showBars(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].show();
+  }
+}
+
+function setup() {
+  createCanvas(width, height);
+  fillArray();
+}
+
+function draw() {
+  background(20);
+  fill(255);
+  // textSize(50);
+  // text(barWidth, 243, 53);
+  showBars(bars);
+  sorter(bars);
+}
+
+function sorter(array) {
+  let swapped = false;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i].h < array[i + 1].h) {
+      let tempH = array[i].h;
+      array[i].h = array[i + 1].h;
+      array[i + 1].h = tempH;
+      swapped = true;
+    }
+  }
+  
+  // If no swaps were made in this pass, the array is sorted
+  if (!swapped) {
+    noLoop();
+  }
+}
+
+class Bar {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  
+  show() {
+    if (this.h % 2 === 0) {
+      fill(200, 100, 0);
+    } else {
+      fill(170, 155, 66);
+    }
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
